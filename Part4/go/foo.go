5578d7692e96b7b15c2ad2d960a1fef6eaa7fdcb
PederDDS <47140994+PederDDS@users.noreply.github.com>
2020-01-17 23:16:40 +0100

    Update foo.go

--- Part4/go/foo.go
+++ Part4/go/foo.go
@@ -12,20 +12,25 @@ var i = 0
 
 func incrementing() {
     //TODO: increment i 1000000 times
+	for x := 0; x<=1000000; x++ {
+	i++
+	}
 }
 
 func decrementing() {
     //TODO: decrement i 1000000 times
+	for k:= 0; k<=1000000; k++ {
+	i--
+	}
 }
 
 func main() {
-    runtime.GOMAXPROCS(runtime.NumCPU())    // I guess this is a hint to what GOMAXPROCS does...
-	                                    // Try doing the exercise both with and without it!
-
+	runtime.GOMAXPROCS(runtime.NumCPU())    
+	runtime.GOMAXPROCS(runtime.NumCPU())                                    
     // TODO: Spawn both functions as goroutines
 	
-    // We have no way to wait for the completion of a goroutine (without additional syncronization of some sort)
-    // We'll come back to using channels in Exercise 2. For now: Sleep.
-    time.Sleep(100*time.Millisecond)
-    Println("The magic number is:", i)
+   	go incrementing()
+    	go decrementing()
+    	time.Sleep(100*time.Millisecond)
+  	Println("The magic number is:", i)
 }
